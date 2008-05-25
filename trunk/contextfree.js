3cee4c671dff530d00c10ac5f8286af21ac9fffe
azaaza <azaaza@7853b292-3e4d-0410-8891-1375a3500784>
2008-05-25 05:44:14 +0000

    Now works with Safari (and probably Opera). Phew. That was gross. Still doesn't work in IE.

--- trunk/contextfree.js
+++ trunk/contextfree.js
@@ -17,8 +17,9 @@ Object.forceExtend = function(dst, src) {
   return dst
 }
 // In case Object.extend isn't defined already, set it to Object.forceExtend.
-if (!Object.extend)
+if (!Object.extend){
   Object.extend = Object.forceExtend
+}  
 
 // Used within a function to get an synonym'ed arguments value, or supply a default.
 // For example:
@@ -42,13 +43,16 @@ function getKeyValue( possibleVariableNames, defaultValue, argList ){
   return defaultValue;
 }
 
-// array-like enumeration
-if (!Array.forEach) { // mozilla already supports this
-  Array.forEach = function(object, block, context) {
-    for (var i = 0; i < object.length; i++) {
-      block.call(context, object[i], i, object);
+// indexOf
+if(!Array.indexOf){
+  Array.prototype.indexOf = function(obj){
+    for(var i=0; i<this.length; i++){
+      if(this[i]==obj){
+        return i;
+      }
     }
-  };
+    return -1;
+  }
 }
 
 // hue, saturation, brightness, alpha
@@ -121,12 +125,13 @@ function Tokenizer( ){
 		var stops = [];
 		var self = this;
 		
-		this._gStopChars.forEach( function( stopChar ) {
+		for( var i=0; i<this._gStopChars.length; i++ ) {
+		  var stopChar = this._gStopChars[i];
 			var foundPos = self._input.indexOf( stopChar, pos );
 			if( foundPos != -1 ){
 				stops.push( foundPos + 1 )
 			}		  
-		})
+		}
 		
 		var m = min(stops);
 		var stopChar = this._gStopChars[ m.index ];
@@ -182,7 +187,7 @@ function Tokenizer( ){
 // TODO: Handle the | argument
 // TODO: Handle comments
 function Compiler() {
-	this._keywords = ["startshape", "rule", "background"]
+	this._keywords = ["startshape", "rule", "background"];
 	this._compiled = {};
 	this._state = null;	
 	var compiler = this;
@@ -414,8 +419,15 @@ Renderer = {
 		Renderer.compiled = compiled;
 		
 		Renderer.canvas = document.getElementById( canvasId );
+
+		//afriendlier way to check to see if we're in IE emulating Canvas 
+    if (typeof(window.G_vmlCanvasManager)!="undefined") { 
+      Renderer.canvas=window.G_vmlCanvasManager.initElement(canvas);
+    }
+                    		
 		Renderer.ctx = Renderer.canvas.getContext("2d");
-		Renderer.width = Renderer.canvas.width;
+		
+    Renderer.width = Renderer.canvas.width;
 		Renderer.height = Renderer.canvas.height;
 		
 		Renderer._globalScale = 300;
@@ -501,9 +513,9 @@ Renderer = {
 		var choices = Renderer.compiled[ruleName];
 		
 		var sum = 0;
-		choices.forEach( function(choice) {
-		  sum += choice.weight;
-		})
+		for( var i=0; i<choices.length; i++) {
+		  sum += choices[i].weight;
+		}
 		
 		var r = Math.random() * sum;
 		
@@ -520,71 +532,110 @@ Renderer = {
 		Renderer.drawShape( shape, transform, color, priority );
 	},
 	
+	_draw: function( transform, drawFunc ) {
+	  // If this is a browser that supports transform and setTransform
+	  // we can use that. It's nice and fast. Currently, the only
+	  // browser to support this is Firefox 3.
+	  if( Renderer.ctx.setTransform ) {
+	    Renderer.setTransform( transform );
+	    drawFunc( Renderer.ctx );
+	    return;
+    }
+    
+    // If the browser does not support setTransform
+	  // then we do a lovely little hack that just
+	  // creates a new canvas (that doesn't have any transformation)
+	  // applied to it; deconvovle the desired transformation into
+	  // rotations, translations, and scales; perform those; and
+	  // finally, draw onto that canvas and blit it onto the output
+	  // canvas. Phew!
+	  
+	  // If we are in FF2, Safari, or Opera... so do the hack :(
+	  var c = document.createElement("canvas");
+	  c.width = Renderer.width;
+	  c.height = Renderer.height;
+	  c.id = "canvas_" + Math.random();
+
+	  var ctx = c.getContext("2d");
+	  var scale = toAffineTransformation( Renderer._globalScale, 0,                0,
+	                                      Renderer._globalScale, Renderer.width/2, Renderer.height/2 );
+	  var trans = compose( scale, transform );
+	  
+	  if( Renderer.ctx.transform ){
+		  ctx.transform( trans[0][0], trans[1][0], trans[0][1], trans[1][1], trans[0][2], trans[1][2] );
+	  } else
+	  {
+      // summary: decompose a 2D matrix into translation, scaling, and rotation components
+      //  The components should be applied in following order:
+      //  | [translate, rotate(angle2), scale, rotate(angle1)]
+      var svd = svdTransform( trans );
+    
+      ctx.translate( svd.dx, svd.dy );
+      ctx.rotate( svd.angle2 );
+      ctx.scale( svd.sx, svd.sy );
+      ctx.rotate( svd.angle1 );
+    }
+    
+	  drawFunc( ctx );
+	  Renderer.ctx.drawImage( c, 0, 0);
+	},
+	
 	drawShape: function( shape, transform, color, priority ){
-	  //console.log( "drawShape: ", shape, color)
-	  shape.draw.forEach( function(item) {
+	  for( i=0; i<shape.draw.length; i++){
+	    var item = shape.draw[i];
+
+			var localTransform = Renderer.adjustTransform( item, transform );
+			var localColor = adjustColor( color, item );
+	    
 			switch( item.shape ){
-				case "CIRCLE":
-					var localTransform = Renderer.adjustTransform( item, transform );
-					Renderer.setTransform( localTransform );
-					
-					var newColor = adjustColor( color, item );
-					
-					Renderer.ctx.beginPath();
-					Renderer.ctx.fillStyle = colorToRgba( newColor );
-					Renderer.ctx.arc( 0, 0, .5, 0, 2*Math.PI, true )
-					Renderer.ctx.fill();
-					Renderer.ctx.closePath();
+				case "CIRCLE":					
+					Renderer._draw( localTransform, function(ctx) {
+  					ctx.beginPath();
+  					ctx.fillStyle = colorToRgba( localColor );
+  					ctx.arc( 0, 0, .5, 0, 2*Math.PI, true )
+  					ctx.fill();
+  					ctx.closePath();					  
+					});
 					break;
 					
 				case "SQUARE":
-					var localTransform = Renderer.adjustTransform( item, transform );
-					Renderer.setTransform( localTransform );
-					
-					var newColor = adjustColor( color, item );
-										
-					Renderer.ctx.beginPath();
-					Renderer.ctx.fillStyle = colorToRgba( newColor );
-					Renderer.ctx.fillRect(-.5, -.5, 1, 1);
-					Renderer.ctx.closePath();
+					Renderer._draw( localTransform, function(ctx) {
+  					ctx.beginPath();
+  					ctx.fillStyle = colorToRgba( localColor );
+  					ctx.fillRect(-.5, -.5, 1, 1);
+  					ctx.closePath();					  
+					});
 					break;
 				
 				case "TRIANGLE":
-					var localTransform = Renderer.adjustTransform( item, transform );
-					Renderer.setTransform( localTransform );
-
-					var newColor = adjustColor( color, item );
-
-					Renderer.ctx.beginPath();
-					var scale = 0.57735; // Scales the side of the triagle down to unit length.
-					Renderer.ctx.moveTo( 0, -scale );
-					for( var i=1; i<=3; i++ ){
-					  Renderer.ctx.lineTo( scale*Math.sin( i*2*Math.PI/3 ), -scale*Math.cos( i*2*Math.PI/3 ) );
-					}
-					Renderer.ctx.fillStyle = colorToRgba( newColor );
-					Renderer.ctx.fill();
-					Renderer.ctx.closePath();
+          Renderer._draw( localTransform, function(ctx) {
+  					ctx.beginPath();
+  					var scale = 0.57735; // Scales the side of the triagle down to unit length.
+  					ctx.moveTo( 0, -scale );
+  					for( var i=1; i<=3; i++ ){
+  					  ctx.lineTo( scale*Math.sin( i*2*Math.PI/3 ), -scale*Math.cos( i*2*Math.PI/3 ) );
+  					}
+  					ctx.fillStyle = colorToRgba( localColor );
+  					ctx.fill();
+  					ctx.closePath();            
+          });
 					break;
 										
 				default:
-
-				  var newColor = adjustColor( color, item );
-					var localTransform = Renderer.adjustTransform( item, transform );
-				  
 				  var threadedDraw = function(shape, transform, color){
 				    this.start = function(){
 				      Renderer.drawRule( shape, transform, color );
 				    }
 				  }
 				  
-				  var tD = new threadedDraw( item.shape, localTransform, newColor );
+				  var tD = new threadedDraw( item.shape, localTransform, localColor );
 				  
 				  if( priority == 1 ){ Renderer.queue.unshift(tD); }
 				  else{ Renderer.queue.push( tD ); }
 				  					
 					break;
 			}	    
-	  });
+	  }
 	},
 	
 	setTransform: function( trans ){
@@ -596,7 +647,7 @@ Renderer = {
 	},
 	
 
-	adjustTransform: function( adjs, transform, fix ){
+	adjustTransform: function( adjs, transform ){
 		// Tranalsation
 		var x = getKeyValue( "x", 0, adjs );
 		var y = -getKeyValue( "y", 0, adjs );
@@ -638,6 +689,219 @@ Renderer = {
 		
 		return transform;
 		
-	}	
-	
+	}		
 }
+
+
+svdTransform  = (function(){
+  //   Copyright (c) 2004-2005, The Dojo Foundation
+  //   All Rights Reserved
+  var m = {}
+  m.Matrix2D = function(arg){
+    // summary: a 2D matrix object
+    // description: Normalizes a 2D matrix-like object. If arrays is passed,
+    //    all objects of the array are normalized and multiplied sequentially.
+    // arg: Object
+    //    a 2D matrix-like object, a number, or an array of such objects
+    if(arg){
+      if(typeof arg == "number"){
+        this.xx = this.yy = arg;
+      }else if(arg instanceof Array){
+        if(arg.length > 0){
+          var matrix = m.normalize(arg[0]);
+          // combine matrices
+          for(var i = 1; i < arg.length; ++i){
+            var l = matrix, r = m.normalize(arg[i]);
+            matrix = new m.Matrix2D();
+            matrix.xx = l.xx * r.xx + l.xy * r.yx;
+            matrix.xy = l.xx * r.xy + l.xy * r.yy;
+            matrix.yx = l.yx * r.xx + l.yy * r.yx;
+            matrix.yy = l.yx * r.xy + l.yy * r.yy;
+            matrix.dx = l.xx * r.dx + l.xy * r.dy + l.dx;
+            matrix.dy = l.yx * r.dx + l.yy * r.dy + l.dy;
+          }
+          Object.extend(this, matrix);
+        }
+      }else{
+        Object.extend(this, arg);
+      }
+    }
+  }
+  // ensure matrix 2D conformance
+  m.normalize = function(matrix){
+      // summary: converts an object to a matrix, if necessary
+      // description: Converts any 2D matrix-like object or an array of
+      //    such objects to a valid dojox.gfx.matrix.Matrix2D object.
+      // matrix: Object: an object, which is converted to a matrix, if necessary
+      return (matrix instanceof m.Matrix2D) ? matrix : new m.Matrix2D(matrix); // dojox.gfx.matrix.Matrix2D
+  }
+  m.multiply = function(matrix){
+    // summary: combines matrices by multiplying them sequentially in the given order
+    // matrix: dojox.gfx.matrix.Matrix2D...: a 2D matrix-like object, 
+    //    all subsequent arguments are matrix-like objects too
+    var M = m.normalize(matrix);
+    // combine matrices
+    for(var i = 1; i < arguments.length; ++i){
+      var l = M, r = m.normalize(arguments[i]);
+      M = new m.Matrix2D();
+      M.xx = l.xx * r.xx + l.xy * r.yx;
+      M.xy = l.xx * r.xy + l.xy * r.yy;
+      M.yx = l.yx * r.xx + l.yy * r.yx;
+      M.yy = l.yx * r.xy + l.yy * r.yy;
+      M.dx = l.xx * r.dx + l.xy * r.dy + l.dx;
+      M.dy = l.yx * r.dx + l.yy * r.dy + l.dy;
+    }
+    return M; // dojox.gfx.matrix.Matrix2D
+  }
+  m.invert = function(matrix) {
+    var M = m.normalize(matrix),
+      D = M.xx * M.yy - M.xy * M.yx,
+      M = new m.Matrix2D({
+        xx: M.yy/D, xy: -M.xy/D, 
+        yx: -M.yx/D, yy: M.xx/D, 
+        dx: (M.xy * M.dy - M.yy * M.dx) / D, 
+        dy: (M.yx * M.dx - M.xx * M.dy) / D
+      });
+    return M; // dojox.gfx.matrix.Matrix2D
+  }
+  // the default (identity) matrix, which is used to fill in missing values
+  Object.extend(m.Matrix2D, {xx: 1, xy: 0, yx: 0, yy: 1, dx: 0, dy: 0});
+
+  var eq = function(/* Number */ a, /* Number */ b){
+    // summary: compare two FP numbers for equality
+    return Math.abs(a - b) <= 1e-6 * (Math.abs(a) + Math.abs(b)); // Boolean
+  };
+  
+  var calcFromValues = function(/* Number */ s1, /* Number */ s2){
+    // summary: uses two close FP values to approximate the result
+    if(!isFinite(s1)){
+      return s2;  // Number
+    }else if(!isFinite(s2)){
+      return s1;  // Number
+    }
+    return (s1 + s2) / 2; // Number
+  };
+  
+  var transpose = function(/* dojox.gfx.matrix.Matrix2D */ matrix){
+    // matrix: dojox.gfx.matrix.Matrix2D: a 2D matrix-like object
+    var M = new m.Matrix2D(matrix);
+    return Object.extend(M, {dx: 0, dy: 0, xy: M.yx, yx: M.xy}); // dojox.gfx.matrix.Matrix2D
+  };
+  
+  var scaleSign = function(/* dojox.gfx.matrix.Matrix2D */ matrix){
+    return (matrix.xx * matrix.yy < 0 || matrix.xy * matrix.yx > 0) ? -1 : 1; // Number
+  };
+  
+  var eigenvalueDecomposition = function(/* dojox.gfx.matrix.Matrix2D */ matrix){
+    // matrix: dojox.gfx.matrix.Matrix2D: a 2D matrix-like object
+    var M = m.normalize(matrix),
+      b = -M.xx - M.yy,
+      c = M.xx * M.yy - M.xy * M.yx,
+      d = Math.sqrt(b * b - 4 * c),
+      l1 = -(b + (b < 0 ? -d : d)) / 2,
+      l2 = c / l1,
+      vx1 = M.xy / (l1 - M.xx), vy1 = 1,
+      vx2 = M.xy / (l2 - M.xx), vy2 = 1;
+    if(eq(l1, l2)){
+      vx1 = 1, vy1 = 0, vx2 = 0, vy2 = 1;
+    }
+    if(!isFinite(vx1)){
+      vx1 = 1, vy1 = (l1 - M.xx) / M.xy;
+      if(!isFinite(vy1)){
+        vx1 = (l1 - M.yy) / M.yx, vy1 = 1;
+        if(!isFinite(vx1)){
+          vx1 = 1, vy1 = M.yx / (l1 - M.yy);
+        }
+      }
+    }
+    if(!isFinite(vx2)){
+      vx2 = 1, vy2 = (l2 - M.xx) / M.xy;
+      if(!isFinite(vy2)){
+        vx2 = (l2 - M.yy) / M.yx, vy2 = 1;
+        if(!isFinite(vx2)){
+          vx2 = 1, vy2 = M.yx / (l2 - M.yy);
+        }
+      }
+    }
+    var d1 = Math.sqrt(vx1 * vx1 + vy1 * vy1),
+      d2 = Math.sqrt(vx2 * vx2 + vy2 * vy2);
+    if(isNaN(vx1 /= d1)){ vx1 = 0; }
+    if(isNaN(vy1 /= d1)){ vy1 = 0; }
+    if(isNaN(vx2 /= d2)){ vx2 = 0; }
+    if(isNaN(vy2 /= d2)){ vy2 = 0; }
+    return {  // Object
+      value1: l1,
+      value2: l2,
+      vector1: {x: vx1, y: vy1},
+      vector2: {x: vx2, y: vy2}
+    };
+  };
+  
+  var decomposeSR = function(/* dojox.gfx.matrix.Matrix2D */ M, /* Object */ result){
+    // summary: decomposes a matrix into [scale, rotate]; no checks are done.
+    var sign = scaleSign(M),
+      a = result.angle1 = (Math.atan2(M.yx, M.yy) + Math.atan2(-sign * M.xy, sign * M.xx)) / 2,
+      cos = Math.cos(a), sin = Math.sin(a);
+    result.sx = calcFromValues(M.xx / cos, -M.xy / sin);
+    result.sy = calcFromValues(M.yy / cos, M.yx / sin);
+    return result;  // Object
+  };
+  
+  var decomposeRS = function(/* dojox.gfx.matrix.Matrix2D */ M, /* Object */ result){
+    // summary: decomposes a matrix into [rotate, scale]; no checks are done
+    var sign = scaleSign(M),
+      a = result.angle2 = (Math.atan2(sign * M.yx, sign * M.xx) + Math.atan2(-M.xy, M.yy)) / 2,
+      cos = Math.cos(a), sin = Math.sin(a);
+    result.sx = calcFromValues(M.xx / cos, M.yx / sin);
+    result.sy = calcFromValues(M.yy / cos, -M.xy / sin);
+    return result;  // Object
+  };
+  
+  return function(transform){
+    // summary: decompose a 2D matrix into translation, scaling, and rotation components
+    // description: this function decompose a matrix into four logical components: 
+    //  translation, rotation, scaling, and one more rotation using SVD.
+    //  The components should be applied in following order:
+    //  | [translate, rotate(angle2), scale, rotate(angle1)]
+    // matrix: dojox.gfx.matrix.Matrix2D: a 2D matrix-like object
+    
+    // First convert the transform into a Dojo-style matrix;
+    var matrix = {
+      xx: transform[0][0],
+      xy: transform[0][1],
+      yx: transform[1][0],
+      yy: transform[1][1],
+      dx: transform[0][2],
+      dy: transform[1][2]
+    };
+    
+    var M = m.normalize(matrix), 
+      result = {dx: M.dx, dy: M.dy, sx: 1, sy: 1, angle1: 0, angle2: 0};
+    // detect case: [scale]
+    if(eq(M.xy, 0) && eq(M.yx, 0)){
+      return Object.extend(result, {sx: M.xx, sy: M.yy});  // Object
+    }
+    // detect case: [scale, rotate]
+    if(eq(M.xx * M.yx, -M.xy * M.yy)){
+      return decomposeSR(M, result);  // Object
+    }
+    // detect case: [rotate, scale]
+    if(eq(M.xx * M.xy, -M.yx * M.yy)){
+      return decomposeRS(M, result);  // Object
+    }
+    // do SVD
+    var MT = transpose(M),
+      u  = eigenvalueDecomposition([M, MT]),
+      v  = eigenvalueDecomposition([MT, M]),
+      U  = new m.Matrix2D({xx: u.vector1.x, xy: u.vector2.x, yx: u.vector1.y, yy: u.vector2.y}),
+      VT = new m.Matrix2D({xx: v.vector1.x, xy: v.vector1.y, yx: v.vector2.x, yy: v.vector2.y}),
+      S = new m.Matrix2D([m.invert(U), M, m.invert(VT)]);
+    decomposeSR(VT, result);
+    S.xx *= result.sx;
+    S.yy *= result.sy;
+    decomposeRS(U, result);
+    S.xx *= result.sx;
+    S.yy *= result.sy;
+    return Object.extend(result, {sx: S.xx, sy: S.yy});  // Object
+  };
+})()
