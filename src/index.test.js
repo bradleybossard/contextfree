4209e8b6efb02cf4260eaa1f2d4b207bcc5c4a8e
Bradley Bossard <bradleybossard@gmail.com>
2016-01-16 20:46:51 +0000

    Fixing some eslint errors

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,9 @@
+/*
 var expect = require('chai').expect;
 var cfdg = require('./index');
 var cfdgsExamples = require('../cfdgs-pretty.json');
 
 describe('cfdg', function() {
-/*
   it('compile', function() {
     var circle = cfdgsExamples['circle'];
     var actual = cfdg.compile(circle);
@@ -11,14 +11,13 @@ describe('cfdg', function() {
     var expected = [ 'startshape', 'c', 'rule', 'c', '{', 'CIRCLE', '{', 's',
                  '3', '}', 'c', '{', 's', '.5', 'b', '.5', 'r', '10', 'x', '1',
                  '}', '}', 'rule', 'c', '{', 'c', '{', 's', '.5', '}', 'c', '{',
-                 's', '.5', 'flip', '100', '}', '}' ]; 
+                 's', '.5', 'flip', '100', '}', '}' ];
 
-    expect(actual).to.have.members(expected); 
+    expect(actual).to.have.members(expected);
   });
-
 
   it('render', function() {
     expect(cfdg.render()).to.be.true;
   });
-*/
 });
+*/
