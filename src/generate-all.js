bbfce8ed9fa8e8a63f57072803022f551bc53a3c
Bradley Bossard <bradleybossard@gmail.com>
2016-05-22 00:32:22 +0000

    Trying async renderings, seems to improve things

--- src/generate-all.js
+++ src/generate-all.js
@@ -3,31 +3,87 @@ var compiler = require('./compiler');
 var renderer = require('./renderer');
 var fs = require('fs');
 var Canvas = require('canvas');
-//var cfdgsExamples = require('../cfdgs-pretty-reproducible.json');
+var async = require('async');
+//var cfdgsExamples = require('../cfdgs-pretty.json');
 var cfdgsExamples = require('../cfdgs-pretty-non-broken.json');
+var keys = Object.keys(cfdgsExamples);
 
-var height = 1200;
-var width = 1000;
+for (var i = 0; i < keys.length; i++) {
+  //if (i == 0 || i == 2) continue;
+  if (i < 7) continue;
+  var key = keys[i];
+  delete cfdgsExamples[key];
+}
+var keys = Object.keys(cfdgsExamples);
+
+var height = 600;
+var width = 600;
 var Image = Canvas.Image;
 
 var tokenized = {};
 var compiled = {};
 var c = new compiler.compiler();
 var r = new renderer.renderer();
+
+async.eachSeries(keys, function(key, callback) {
+  //var canvas = null;
+  var canvas = new Canvas(width, height);
+  var grammar = cfdgsExamples[key];
+  var tokens = tokenizer.tokenize(grammar);
+  tokenized[i] = tokens.slice(0);
+
+  var compiledTree = c.compile(tokens);
+  compiled[i] = compiledTree; 
+
+  var filename = 'output/' + key + '.png';
+  console.log(filename + ' started');
+  //var render = r.render(compiledTree, canvas, 0.24);
+  var render = r.render(compiledTree, canvas);
+
+  canvas.toBuffer(function(err, buf) {
+    console.log(buf);
+    fs.writeFile(filename, buf, function() {
+      console.log(filename + ' done');
+      gc();
+      callback();
+    });
+  });
+});
+
+return;
+
+/*
+function writeToCanvas(canvas, filename) {
+  console.log(canvas);
+  canvas.toBuffer(function(err, buf) {
+    console.log(buf);
+    fs.writeFile(filename, buf, function() {
+      //console.log('');
+      gc();
+    });
+  });
+}
+
 for (var i in cfdgsExamples) {
   console.log(i);
-  var canvas = new Canvas(width, height);
-  var ctx = canvas.getContext('2d');
+  //var canvas = new Canvas(width, height);
+  canvases[i] = new Canvas(width, height);
+  //canvas = new Canvas(width, height);
+  //var ctx = canvas.getContext('2d');
   var grammar = cfdgsExamples[i];
   var tokens = tokenizer.tokenize(grammar);
   tokenized[i] = tokens.slice(0);
-  //compiled[i] = c.compile(tokens);
   var compiledTree = c.compile(tokens);
   compiled[i] = compiledTree; 
   console.log(compiledTree);
-  var render = r.render(compiledTree, canvas, 0.24);
-  fs.writeFile('output/' + i + '.png', canvas.toBuffer());
+  //var render = r.render(compiledTree, canvas, 0.24);
+  var render = r.render(compiledTree, canvas);
+  gc();
+  var filename = 'output/' + i + '.png';
+  writeToCanvas(canvases[i], filename); 
 }
+*/
+
 /*
 var tokenizedString = JSON.stringify(tokenized);
 var compiledString = JSON.stringify(compiled);
