0af402fdebb826b178d1a7b435ed560902db2e7a
Bradley Bossard <bradleybossard@gmail.com>
2016-06-26 22:40:52 +0000

    Fixed some eslint warnings

--- src/index.js
+++ src/index.js
@@ -3,16 +3,15 @@ var renderer = require('./renderer.js');
 
 function compile(grammar) {
   var c = new compiler.compiler();
-  return(c.compile(grammar));
+  return c.compile(grammar);
 }
 
 function render(compiled, canvas, canvasSize) {
-  //var r = new renderer.renderer();
-  // TODO(bradleybossard): Do I need canvas size?
+  // TODO: Do I need canvas size?
   renderer.render(compiled, canvas, canvasSize);
 }
 
 module.exports = {
   compile: compile,
-  render: render,
+  render: render
 };
